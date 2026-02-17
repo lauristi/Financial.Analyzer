041d83543dcfebfddbcd1c2307b3558e5e535d55
sysdba <sysdba@ASUSPC>
2026-02-17 08:32:18 -0300

    Build: Removendo artefatos de compilação e arquivos de usuário do rastreamento

--- Server.Solution/Server.Domain/obj/Debug/net8.0/Server.Domain.AssemblyInfo.cs
+++ Server.Solution/Server.Domain/obj/Debug/net8.0/Server.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88fc6daef055f07764991652dc30aae4c3a77a4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4240613d3a2a25aea20b7faf75a256cd08c8c935")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
